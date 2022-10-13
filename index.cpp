72315188a848456d25b6b7817f995561f959ee6f
sanyaswb <sanyaswb@gmail.com>
2022-10-14 01:22:35 +0300

    pawn

--- index.cpp
+++ index.cpp
@@ -2,5 +2,5 @@
 #include <string>
 
 bool pawn(int x1, int y1, int x2, int y2) {
-	return false;
+	return x2 == x1 - 1 && y2 == y1 + 1 || x2 == x1 + 1 && y2 == y1 - 1 || x2 == x1 + 1 && y2 == y1 + 1 || x2 == x1 - 1 && y2 == y1 - 1 ? true : false;
 }
