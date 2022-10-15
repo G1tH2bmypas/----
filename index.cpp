fd91be10a3c68fc1f47d3c650a75bfa717accc71
Tekyto <Tekyto2014@gmail.com>
2022-10-15 13:55:06 +0300

    Peshka

--- index.cpp
+++ index.cpp
@@ -2,5 +2,5 @@
 #include <string>
 
 bool pawn(int x1, int y1, int x2, int y2) {
-	return x2 == x1 - 1 && y2 == y1 + 1 || x2 == x1 + 1 && y2 == y1 + 1;
+	return x2 == x1 + 1 && y2 == y1 + 1 || x2 == x1 - 1 && y2 == y1 + 1;
 }
